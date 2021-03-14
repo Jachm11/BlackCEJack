8b1fb8540d140b35976b15d082df10bf77e2cb11
Natalia GB <natigonzalezbermudez@gmail.com>
2021-03-13 19:52:21 -0600

    draw_card para la interfaz

--- Pruebas de lógica/pruebas.rkt
+++ Pruebas de lógica/pruebas.rkt
@@ -1,11 +1,28 @@
 
 #lang racket/gui
+(provide draw_card)
+(provide keep_playing?)
+(provide winners)
+(provide bCEj)
+(provide first)
+(provide rest)
+(provide second)
+(provide third)
 
-[define (replace_from_list ele x lista)
-  (cond ((null? lista) '())
-        ((equal? ele (car lista)) (append (list x) (replace_from_list ele x (cdr lista))))
-        (else (append (list (car lista)) (replace_from_list ele x (cdr lista))))
-  )
+(define (first list)
+  (car list))
+(define (rest list)
+  (cdr list))
+(define (second list)
+  (cadr list))
+(define (third list)
+  (caddr list))
+
+[define (replace_as_value hand)
+  (cond ((null? hand) '())
+        ((as? (car hand)) (append (list 11.1) (cdr hand)))
+        (else (append (list (car hand)) (replace_as_value (cdr hand)))))
+  
  ]
 [define (delete_last_element lista)
   (reverse (cdr (reverse lista)))
@@ -48,10 +65,25 @@
 [define (draw_cards num deck)
   (list (delete_last_element (draw_cards_aux num deck)) (get_last_element (draw_cards_aux num deck)))]
 [define (draw_cards_aux num deck)
-  (cond ((equal? num 0) (list deck)) ;;(list deck))
+  (cond ((equal? num 0) (list deck)) 
         (else (append (list (car deck)) (draw_cards_aux (- num 1) (cdr deck)))
               ))]
+;;funcion (draw_card jugador juego_completo) para interfaz (deck, crupier, primero, segundo, tercero)
+[define (draw_card player game)
+  (replace 0 (second (draw_cards 1 (first game))) (replace (+ player 1) (append (first (draw_cards 1 (first game))) (list-ref game (+ player 1))) game))
+  ]
 
+
+[define (replace index ele lista)
+  (cond((<= (length lista) index) lista)
+       ((equal? index 0) (append (list ele) (cdr lista)))
+        (else (append (list (car lista)) (replace (- index 1) ele (cdr lista)))))]
+
+
+  
+(replace 0 "a" '(1 2 3 4 5 6)) 
+
+        
 ;;( (1 (5 4)) (2 (1 10)) (3 (10 11)) )  (jugador (carta1 carta2...))
 
 ;; si la carta es un as
@@ -86,30 +118,43 @@
         ((as? (car hand)) #t)
         (else (has_as? (cdr hand))))]
 ;;sumar cartas
-[define (add_cards set)
-  (add_cards_aux set)]
+[define (add_cards hand)
+  (cond ((and (has_as? hand)(> (add_cards_aux hand) 21)) (add_cards (replace_as_value hand)))
+        (else (add_cards_aux hand)))]
   
 
-[define (add_cards_aux set)  
-  (cond((null? set) 0)
-      (else (+ (get_card_value (car set)) (add_cards_aux (cdr set)))))]
+[define (add_cards_aux hand)  
+  (cond((null? hand) 0)
+      (else (+ (get_card_value (car hand)) (add_cards_aux (cdr hand)))))]
+
+
 
 [define (keep_playing? hand)
-  (cond ((> (add_cards hand) 21) #f)
+  (cond ((>= (add_cards hand) 21) #f)
         (else #t))]
-;;[define (
+
 
 [define (winners game)
   (winners_aux (car game) (cdr game))  
   ]
 [define (winners_aux crupie players)
   (cond ((null? players) '())
-        ((blackjack? (car players)) (append (list "Won") (winners_aux crupie (cdr players))))
-        ((not (keep_playing? crupie)) (append (list "Won") (winners_aux crupie (cdr players))))
+        ((blackjack? (car players)) (append (list "BlackJack") (winners_aux crupie (cdr players))))
+        ((blackjack? crupie) (append (list "Crupier BlackJack") (winners_aux crupie (cdr players))))
         ((not (keep_playing? (car players))) (append (list "Lost") (winners_aux crupie (cdr players))))
+        ((not (keep_playing? crupie)) (append (list "Won") (winners_aux crupie (cdr players))))
         ((< (add_cards (car players)) (add_cards crupie)) (append (list "Lost") (winners_aux crupie (cdr players))))
+        ((= (add_cards (car players)) (add_cards crupie)) (append (list "Tie") (winners_aux crupie (cdr players))))
         (else (append (list "Won") (winners_aux crupie (cdr players)))))]
-         
+
+;;crupier tiene blackjack
+(winners '((121 12) (12 65) (12 13) (121 64 131)))
+
+;; Jugador 1 con blackjack
+(winners '((121 12) (121 12) (12 13) (121 64 131)))
+
+;;j1 empata con 12, j2 gana con 13, j3 pierde con 10
+(winners '((12 12) (12 12) (34 113) (64 43)))
                      
   
   
