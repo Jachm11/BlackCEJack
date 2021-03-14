2a738e95b742e9fab88e1ec4285cc66db2d8662a
Jose Alejandro Chavarria <joalchama@gmail.com>
2021-03-13 20:20:00 -0600

    Union gui logica

---
+++ Pruebas de interfaz/logic.rkt
@@ -0,0 +1,24 @@
+#lang racket
+
+(provide draw_card)
+(provide keep_playing?)
+(provide winners)
+(provide bCEj)
+
+(define (draw_card player game)
+  '((82 103 132 11) (12 73 41 13) (91 33) (81 54 71)))
+
+(define (keep_playing? cards)
+  #t)
+
+(define (winners cards)
+  '("WINS" "LOSES" "WINS"))
+
+
+(define (bCEj players)
+  '( (11 22 34 545 6787 80 3454 65)(82 103) (12 73) (91 33) (81 54)))
+
+
+  
+
+
