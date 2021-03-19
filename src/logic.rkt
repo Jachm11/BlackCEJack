f8760baec7f2b7deb3683207ec8a44d2ea3cd201
Natalia GB <natigonzalezbermudez@gmail.com>
2021-03-19 12:38:13 -0600

    corrección de error en lost?

--- src/logic.rkt
+++ src/logic.rkt
@@ -125,7 +125,7 @@
 [define (lost? crupier player)
   (cond((or (and (not (keep_playing? player))
        (not (equal? (add_cards player) 21)))
-       (and (keep_playing? crupier)
+       (and (or (keep_playing? crupier) (equal? (add_cards player) 21))
             (< (add_cards player) (add_cards crupier)))) #t)
   (else #f))
   
